2fb0580ec9c05e7c8d92e994b5cc3f3665028bba
Isaac Lessard <IsaacL@Isaacs-MacBook-Air-2.local>
2015-11-17 12:52:39 -0700

    removed bootstrap, added gulp scss task

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,20 @@
 var gulp = require('gulp');
 var mocha = require('gulp-mocha');
+var sass = require('gulp-sass');
 
-gulp.task('default', ['test']);
+gulp.task('default', ['sass']);
 
 gulp.task('test', function (){
 	return gulp.src('./test/*.js', {read: false})
 	.pipe(mocha())
+})
+
+gulp.task('sass', function(){
+	gulp.src('./scss/*.scss')
+		.pipe(sass().on('error', sass.logError))
+		.pipe(gulp.dest('./css'))
+})
+
+gulp.task('sass:watch', function(){
+	gulp.watch('./scss/*.scss', ['sass']);
 })
