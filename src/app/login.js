b5075d0ac1cd6dee96de650a9c391b9ca5c01289
Isaac Lessard <IsaacL@Isaacs-MacBook-Air-2.local>
2015-11-18 10:52:59 -0700

    log in feature works, relocates to foodie.html

--- src/app/login.js
+++ src/app/login.js
@@ -9,8 +9,30 @@ $(document).ready(function(){
     }
     localStorage.setItem('user',JSON.stringify($user));
   }
-  
+
   var userSet = document.getElementById('signUpButton');
   userSet.addEventListener('click',setLogin);
+
+  function logIn(){
+    var $login = JSON.parse(localStorage.getItem('user'));
+    console.log($login);
+   var $usernameInput = document.getElementById('logUsername').value;
+   var $passwordInput = document.getElementById('logPassword').value;
+   console.log($login.username);
+   console.log($usernameInput);
+   console.log($login.password);
+   console.log($passwordInput);
+   if (
+     $usernameInput == $login.username && $passwordInput == $login.password
+   ){
+     console.log("Logged In!");
+     window.location.href = "foodie.html";
+   } else {
+     console.log("Not logged in");
+   }
+  }
+
+  var logUserIn = document.getElementById('logInButton');
+  logUserIn.addEventListener('click',logIn);
 
 });
