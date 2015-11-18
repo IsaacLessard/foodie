34e3e4fe659dc1a19f7f86a39e4149b43ecfca83
Isaac Lessard <IsaacL@Isaacs-MacBook-Air-2.local>
2015-11-18 11:32:01 -0700

    when search is hit, recipes are deleted and new search populates page

--- src/app/recipe.js
+++ src/app/recipe.js
@@ -9,16 +9,31 @@ $(document).ready(function(){
     document.body.appendChild(source);
   }
 
-  function search(){
+  var $urlBase = "http://www.recipepuppy.com/api/?p=";
+  var $pageIndex = 1;
+
+
+  var increaseIndex = document.getElementById('next');
+  next.addEventListener('click',function(){
+      $pageIndex += 1;
+      search();
+  })
+
+  var $recipeLayout = document.getElementById('displayRecipes');
+
+
+  function search(pageIndex){
+    var $searchTerm = "&q=" + document.getElementById('recipeSearch').value;
+    var $urlTotal = $urlBase + $pageIndex + $searchTerm;
     console.log("search");
-    var $searchTerm = document.getElementById('recipeSearch').value;
-    var $url = "http://www.recipepuppy.com/api?q="+$searchTerm;
+    var $url = $urlTotal;
     var request = $.ajax({
       method: "GET",
       url: $url,
       dataType: "json",
       success: function(data){
         console.log(data);
+        console.log($urlTotal);
         for (j=0;j<data.results.length;j++){
           var $source = document.createElement('li');
           var $title = data.results[j].title;
@@ -26,20 +41,29 @@ $(document).ready(function(){
           var $thumbnail = data.results[j].thumbnail;
           var $ingredients = data.results[j].ingredients;
           $source.innerHTML = "<div class='recipeShow'><h2>"
-          +$title+"</h2><a href='" 
+          +$title+"</h2><a href='"
           + $href + "'>View Recipe</a>"
           + "<img src='"+$thumbnail+"'>"
           + "<p>"+$ingredients+"</p>"
           + "</div>";
           console.log($title);
-          document.body.appendChild($source);
+          $recipeLayout.appendChild($source);
+          }
         }
-      }
-    })
-  }
+      })
+    }
+
+    function clearSearch(){
+      $recipeLayout.innerHTML = "";
+    }
+
+    function searchAll(){
+      clearSearch();
+      search();
+    }
 
   var recipeSearch = document.getElementById('recipeSearchButton');
-  recipeSearch.addEventListener('click',search);
+  recipeSearch.addEventListener('click',searchAll);
 
 
 
