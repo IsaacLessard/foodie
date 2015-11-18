c8a0f89db7a08e2affb4e1994deea243d737d515
Isaac Lessard <IsaacL@Isaacs-MacBook-Air-2.local>
2015-11-18 08:19:43 -0700

    started ajax request recipe js

--- src/app/contentRotate.js
+++ src/app/contentRotate.js
@@ -1,22 +1,4 @@
 $(document).ready(function(){
-  // $.get('http://www.recipepuppy.com/api/?i=onions,garlic&q=omelet&p=3')
-  //   .done(function(data){
-  //     localstorage.setItem('pupData', JSON.stringify(data));
-  //     appendImagesToMain(data);
-  //   })
-  //   .fail(function(){
-  //   console.log('Failed!');
-  //   var offLineData = JSON.parse( localStorage.getItem('pupData') );
-  //   appendImagesToMain(offLineData);
-  // })
-  //
-  // appendImagesToMain = function(data){
-  //   var $imageArea = $('#imageArea');
-  //   for(i=0;i<data.results.length;i++){
-  //     console.log(data.results[i].title);
-  //     $imageArea.append('<img src="'+data.results[i].thumbnail+'">');
-  //   }
-  // }
 
   $('#logIn').click(function(){
     console.log("clicked!");
---
+++ src/app/recipe.js
@@ -0,0 +1,24 @@
+$(document).ready(function(){
+
+  var listArray = [];
+  for(k=0;k<listArray.length;k++){
+    var source = document.createElement('li');
+    source.innerText = myArray[k];
+    document.body.appendChild(source);
+  }
+
+  function search(){
+    var $searchTerm = document.getElementById('recipeSearch').value;
+    var $url = "http://www.recipepuppy.com/api?q="+$searchTerm;
+    var request = $.ajax({
+      method: "GET",
+      url: $url,
+      dataType: "json",
+    })
+  }
+
+
+
+
+
+});
