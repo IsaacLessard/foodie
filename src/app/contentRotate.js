d8d7cc75448a7b261b796345b317fafcc38aa7e7
Isaac Lessard <IsaacL@Isaacs-MacBook-Air-2.local>
2015-11-18 07:44:42 -0700

    added styles to index and login

--- src/app/contentRotate.js
+++ src/app/contentRotate.js
@@ -18,6 +18,20 @@ $(document).ready(function(){
   //   }
   // }
 
+  $('#logIn').click(function(){
+    console.log("clicked!");
+    $('.logIn').addClass('display');
+    $('.signUp').removeClass('display');
+  });
+
+
+  $('#signUp').click(function(){
+    console.log("clicked!");
+    $('.signUp').addClass('display');
+    $('.logIn').removeClass('display');
+  });
+
+
   $('#entry').click(function(){
      console.log("clicked!");
     $('.entry').addClass('display');
