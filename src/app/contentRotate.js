66bb8a54e416256bf15b51c04362ad58cde78bfb
Isaac Lessard <IsaacL@Isaacs-MacBook-Air-2.local>
2015-11-18 07:15:14 -0700

    added rotating content on foodie.html

--- src/app/contentRotate.js
+++ src/app/contentRotate.js
@@ -1,20 +1,43 @@
-$(function(){
-  $.get('http://www.recipepuppy.com/api/?i=onions,garlic&q=omelet&p=3')
-    .done(function(data){
-      localstorage.setItem('pupData', JSON.stringify(data));
-      appendImagesToMain(data);
-    })
-    .fail(function(){
-    console.log('Failed!');
-    var offLineData = JSON.parse( localStorage.getItem('pupData') );
-    appendImagesToMain(offLineData);
-  })
+$(document).ready(function(){
+  // $.get('http://www.recipepuppy.com/api/?i=onions,garlic&q=omelet&p=3')
+  //   .done(function(data){
+  //     localstorage.setItem('pupData', JSON.stringify(data));
+  //     appendImagesToMain(data);
+  //   })
+  //   .fail(function(){
+  //   console.log('Failed!');
+  //   var offLineData = JSON.parse( localStorage.getItem('pupData') );
+  //   appendImagesToMain(offLineData);
+  // })
+  //
+  // appendImagesToMain = function(data){
+  //   var $imageArea = $('#imageArea');
+  //   for(i=0;i<data.results.length;i++){
+  //     console.log(data.results[i].title);
+  //     $imageArea.append('<img src="'+data.results[i].thumbnail+'">');
+  //   }
+  // }
 
-  appendImagesToMain = function(data){
-    var $imageArea = $('#imageArea');
-    for(i=0;i<data.results.length;i++){
-      console.log(data.results[i].title);
-      $imageArea.append('<img src="'+data.results[i].thumbnail+'">');
-    }
-  }
-})
+  $('#entry').click(function(){
+     console.log("clicked!");
+    $('.entry').addClass('display');
+    $('.recipes').removeClass('display');
+    $('.diary').removeClass('display');
+  });
+
+  $('#recipes').click(function(){
+    console.log("clicked!");
+    $('.recipes').addClass('display');
+    $('.entry').removeClass('display');
+    $('.diary').removeClass('display');
+  });
+
+  $('#diary').click(function(){
+    console.log("clicked!");
+    $('.diary').addClass('display');
+    $('.recipes').removeClass('display');
+    $('.entry').removeClass('display');
+  });
+
+
+});
