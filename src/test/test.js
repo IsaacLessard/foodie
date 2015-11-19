faee3d0b36600e7ba6215f5416d79da6890542ee
Isaac Lessard <IsaacL@Isaacs-MacBook-Air-2.local>
2015-11-19 14:47:40 -0700

    removed test

--- src/test/test.js
+++
@@ -1,8 +0,0 @@
-var main = require('../app/main');
-var assert = require('assert');
-
-describe('phi', function () {
-	it('should equate the correct correlation for a given table', function () {
-		assert.equal(phi,([76,9,4,1]), 7)
-	})
-})
